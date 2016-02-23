25ca83e23ffbc8248c1551eb007f5ca1ae342680
BiBzz <alexandre.berrebi@businessagile.eu>
2016-02-23 18:19:33 +0100

    code optimization

--- bissextile.py
+++ bissextile.py
@@ -4,16 +4,7 @@
 bissextile = False
 annee = int(input("Saisissez une année : "))
 
-if annee % 400 == 0:
-	bissextile = True
-elif annee % 100 == 0:
-	bissextile = False
-elif annee % 4 == 0:
-	bissextile = True
-else:
-	bissextile = False
-
-if bissextile == True:
+if annee % 400 == 0 or (annee % 4 == 0 and annee % 100 != 0):
 	print(annee, "est un année bissextile")
 else:
 	print(annee, "n'est pas un année bissextile")
