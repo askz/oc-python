54f8e124ea883ee47a5472b6322ecd552acba8bd
Max <askz@users.noreply.github.com>
2017-06-14 14:54:08 +0200

    Typo

--- bissextile.py
+++ bissextile.py
@@ -4,6 +4,6 @@
 annee = int(input("Saisissez une année : "))
 
 if annee % 400 == 0 or (annee % 4 == 0 and annee % 100 != 0):
-	print(annee, "est un année bissextile")
+	print(annee, "est une année bissextile")
 else:
-	print(annee, "n'est pas un année bissextile")
+	print(annee, "n'est pas une année bissextile")
